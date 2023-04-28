c761034a8cd73adcf7be27e355c96895f15aa2b9
tkoneru <tkoneru@iu.edu>
2023-04-28 14:35:05 -0400

    orderservice-frontend

---
+++ orderservice-frontend/components/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    
+    return (
+        <>
+        <div className={styles.container}>
+           footer
+        </div>
+         
+        </>
+    )
+}
+
+export default Footer;
---
+++ orderservice-frontend/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Demo</title>
+        <meta name="description" content="Demop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      
+    </>
+  )
+}
