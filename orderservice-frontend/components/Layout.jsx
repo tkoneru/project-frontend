87e05d24121554b41dcf598f753a652cc0be9829
tkoneru <tkoneru@iu.edu>
2023-04-28 18:08:49 -0400

    orderservice-frontend update

--- orderservice-frontend/components/Layout.jsx
+++ orderservice-frontend/components/Layout.jsx
@@ -1,9 +1,10 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import styles from "@/styles/Home.module.css";
 
 
 const Layout = ({children}) => {
-    
+
     return (
         <>
          <NavBar />
--- orderservice-frontend/pages/index.js
+++ orderservice-frontend/pages/index.js
@@ -14,8 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the order service.</h1>
-
+        <div className={styles.container}>
+            Welcome to order service.
+        </div>
     </>
   )
 }
